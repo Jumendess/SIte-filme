20e291d70d12001ed8dbec1859a89de880f584f8
Julio mendes <julio.mendes60@gmail.com>
2023-08-16 15:04:57 -0300

    ajustando paginas

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,4 @@
-  const videos = [
-    {
-      title: "Somos Todos Iguais",
-      src: "https://i.imgur.com/6k74YUm.mp4",
-      thumbnail: "images/SomosTodosIguias.jpg"
-    },
-    {
-      title: "Caça as Bruxas",
-      src: "https://www.example.com/video2.mp4",
-      thumbnail: "images/cacaBruxas.jpg"
-    },
-    {
-      title: "Sem Limites",
-      src: "https://www.example.com/video3.mp4",
-      thumbnail: "images/semLimites.jpg"
-    },
-    {
-      title: "Vídeo 4",
-      src: "https://www.example.com/video4.mp4",
-      thumbnail: "images/missao-impossivel-poste.jpg" // Substitua pela imagem do vídeo 4
-    },
-    {
-      title: "Vídeo 5",
-      src: "https://www.example.com/video5.mp4",
-      thumbnail: "images/medio.jpg" // Substitua pela imagem do vídeo 5
-    },
-    {
-      title: "Vídeo 6",
-      src: "https://www.example.com/video6.mp4",
-      thumbnail: "images/dungeons-e-dragons.jpg" // Substitua pela imagem do vídeo 6
-    }
-  ];
 
-const gridContainer = document.querySelector('.grid-container');
-
-videos.forEach(videoInfo => {
-  const videoItem = document.createElement('div');
-  videoItem.classList.add('grid-item');
-
-  const thumbnail = document.createElement('img');
-  thumbnail.src = videoInfo.thumbnail;
-  thumbnail.classList.add('video-thumbnail');
-
-  const titleElement = document.createElement('div');
-  titleElement.classList.add('video-title');
-  titleElement.textContent = videoInfo.title;
-
-  const watchButton = document.createElement('button');
-  watchButton.textContent = 'Assistir';
-  watchButton.addEventListener('click', () => {
-    videoItem.removeChild(thumbnail);
-    videoItem.removeChild(titleElement);
-    videoItem.removeChild(watchButton);
-
-    const video = document.createElement('video');
-    video.controls = true;
-    video.classList.add('video-item');
-    const source = document.createElement('source');
-    source.src = videoInfo.src;
-    source.type = 'video/mp4';
-    video.appendChild(source);
-
-    videoItem.appendChild(video);
-    video.play();
-  });
-
-  videoItem.appendChild(thumbnail);
-  videoItem.appendChild(titleElement);
-  videoItem.appendChild(watchButton);
-  gridContainer.appendChild(videoItem);
-});
 //////////////////////////////////////////////////////////////////////////////
 //Tela de inicio
 
@@ -79,7 +9,7 @@ function validateAndLogin() {
   if (email === "" || password === "") {
     alert("Por favor, preencha todos os campos.");
   } else {
-    alert(`Email: ${email}\nSenha: ${password}`);
+    // alert(`Email: ${email}\nSenha: ${password}`);
     window.location.href = "teste.html"; // Redireciona para a página index.html
   }
 }
